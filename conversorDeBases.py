e49dfc3bc3d524b1f19ba987cfda02ccba6463bf
diogo-speck <diogospeck@gmail.com>
2026-05-29 11:35:09 -0300

    decimal, binario e octal funcionando exceto 0

--- conversorDeBases.py
+++ conversorDeBases.py
@@ -7,13 +7,47 @@ def converterDecimalBinario(num):
         num = num // 2
     restos = restos[::-1] # inverte a lista
     binario = "".join(str(r) for r in restos) # converte lista em String com .join
-    return binario if binario else "0" # se binário existe, retorna, se não retorna 0
+    binario = int(binario)
+    return binario if binario else 0 # se binário existe, retorna, se não retorna 0
 
 def converterBinarioDecimal(num):
     valor = 0
-    for n in range(len(num)):
-        valor +=num[n]*(2**n)
+    texto = str(num) # transforma o número em uma string
+    lista = list(texto)[::-1] # pega o texto e coloca em uma lista, invertendo logo após
+    for n, digito in enumerate(lista): # para n, sendo n todos os digitos(em texto) na lista pegue o índice e o valor(elemento naquela posição) ao mesmo tempo com a função enumerate
+        valor += int(digito)*(2**n) # pega o valor(digito, 0 ou 1) e transforma em int para multiplicar com 2 elevado a n, sendo n o índice
     return valor
+
+def converterDecimalOctal(num):
+    restos = []
+    while num > 0:
+        resto = num % 8
+        restos.append(resto)
+        num = num // 8
+    restos = restos[::-1] # inverte a lista
+    octal = "".join(str(r) for r in restos) # converte lista em String com .join
+    octal = int(octal)
+    return octal if octal else 0 # se octal existe, retorna, se não retorna 0
+
+def converterBinarioOctal(num):
+    numero = converterBinarioDecimal(num)
+    octal = converterDecimalOctal(numero)
+    return octal
+
+def converterOctalBinario(num): # arrumar
+    valor = []
+    lista = list(str(num))
+    for n, digito in enumerate(lista): # para n, sendo n todos os digitos(inteiros) na lista pegue o índice e o valor(elemento naquela posição) ao mesmo tempo com a função enumerate
+        binario = converterDecimalBinario(int(digito))
+        valor.append(str(binario).zfill(3)) # garante 3 bits agrupando eles de 3 em 3
+    octal = "".join(str(r) for r in valor) # converte lista em String com .join
+    return int(octal) # converte String para int
+
+def converterOctalDecimal(num): # arrumar
+    numero = converterOctalBinario(num)
+    decimal = converterBinarioDecimal(numero)
+    return decimal
+
 
 continua = True
 while (continua):
@@ -34,6 +68,7 @@ while (continua):
                     binario = converterDecimalBinario(decimal)
                     print(binario)
                 case "o":
+                    octal = converterDecimalOctal(decimal)
                     print(octal)
                 case "h":
                     print(hexa)
@@ -47,6 +82,7 @@ while (continua):
                 case "b":
                     print(binario)
                 case "o":
+                    octal = converterBinarioOctal(binario)
                     print(octal)
                 case "h":
                     print(hexa)
@@ -55,8 +91,10 @@ while (continua):
             escolha = input("Deseja converter para qual tipo? (d,b,o,h) ")
             match escolha:
                 case "d":
+                    decimal = converterOctalDecimal(octal)
                     print(decimal)
                 case "b":
+                    binario = converterOctalBinario(octal)
                     print(binario)
                 case "o":
                     print(octal)
