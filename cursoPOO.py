4f2d953c83d003e262b36944f099fcb1e172fc9e
diogo-speck <diogospeck@gmail.com>
2026-06-17 12:30:41 -0300

    Redução e correção dos comentários para começar a Aula 04

--- cursoPOO.py
+++ cursoPOO.py
@@ -1,21 +1,17 @@
-# Comentários sobre o curso "Python Orientado a Objetos" do canal Curso em Vídeo
-# A "Crise de Software" surgiu em 1960
-# A característica que identifica as linguagens lineares são os desvios forçados com o "goto"
-# O Criador da linguagem "Smalltalk" foi Alan Kay
-# A linguagem simula foi um super conjunto da linguagem Algol60
-#  As duas últimas letras da sigla "OOAD" significam Analysis/Design
-# COMERN C-confiavel, O-oportuno, M-manutenivel, E-extensivo, R-reutilizavel e N-Natural
-# Alterações não muda outra área; Pode ser feito simultaneamente; Mais fácil de atualizar;
-# Sistema não estático (tudo pode crescer de forma útil); Utilizado em outros sistemas
-# Mais fácil de entender
-# (Classe = Molde) != Objeto
-# Nome, +-# atributos (coisas que a classe vai ter) e métodos +-# (coisas que eu posso fazer com a classe) ()
-# Instância = ação de instanciar/seguir o padrão da classe para criar o objeto
-# "Um objeto é uma instância de uma classe, definido por algo material/abstrato a partir de uma classe, descrito por seus atributos, métodos e estado"
-# Todo atributo tem valores de um estado
-# Ex. Objeto biscoito, instanciar a Classe molde de coração (colocar no molde), atributo inteiro, estado verdadeiro, método comer (muda o estado)
-# Atividade: Objeto1(concreto)-computador, classe marca hp, atributo ligado, estado verdadeiro / Objeto2(abstrato)-previsão do tempo, classe dia limpo, atributo chuva, estado falso
-# 
+'''
+Python enables programs to be written compactly and readably. Programs written in Python are typically much shorter than equivalent C, C++, or Java programs, for several reasons:
+    - the high-level data types allow you to express complex operations in a single statement;
+    - statement grouping is done by indentation instead of beginning and ending brackets;
+    - no variable or argument declarations are necessary.
+'''
+# Todo número resultante de uma divisão é um tipo float (floating-point number)
+# ToDo floor division and get just the integer result you can use the //
+# Todo intervalo segue esse padrão (começo(incluso), fim(não-incluso), o quanto que vai pular)
+# Todo tipo de função cria seu próprio espaço de variáveis
+# Todo tipo de função sempre retorna algo, apesar de não ter um return, ela ainda retorna algo nulo. Esse é um padrão que todo tipo de dado mutável em python segue
+# Todo método em Python dentro de uma classe deve receber self como primeiro parâmetro, que representa a própria instância
+# Todo tipo de classe precisa ter um método especial chamado __init__() por padrão
+# Todos os métodos de python são do tipo virtual como o de C++
 
 import random
 from collections import deque
@@ -377,110 +373,21 @@ for char in reverse(w):
     print(char, end='')
 list(w[i] for i in range(len(w)-1, -1, -1))
 
-# Python enables programs to be written compactly and readably. Programs written in Python are typically much shorter than equivalent C, C++, or Java programs, for several reasons:
-# - the high-level data types allow you to express complex operations in a single statement;
-# - statement grouping is done by indentation instead of beginning and ending brackets;
-# - no variable or argument declarations are necessary.
-# Todo número resultante de uma divisão é um tipo float (floating-point number)
-# ToDo floor division and get just the integer result you can use the //
-# entre operações de diferentes tipos de variáveis, predomina aquela que engloba a outra nos conjuntos ex. float(int)
-# Strings usam "" ou '' para concatenar caracteres especiais usa-se \(alt 92) "\"" \n pula linha
-# print r serve para não realizar as ações usando \ (raw string)
-# print(""" """) serve para fazer um print formatado com multiplas linhas
-# String em python é um array que pode ter seus caracteres citados usando o seu índice inclusive negativamente voltando ex. variavel[-n]
-# Note that since -0 is the same as 0, negative indices start from -1
-# In addition to indexing, slicing is also supported. While indexing is used to obtain individual characters, slicing allows you to obtain a substring variavel[n:i]
-# Python strings cannot be changed — they are immutable
-# Strings are examples of sequence types, and support the common operations supported by such types
-# Listas são mutáveis e por isso podem sofrer alterações manualmente pelo seu index ou pelo método list.append()
-# Uma variável nunca copia o dado, ela só o referencia, no caso de uma lista ela vai referenciar aos mesmos itens
-# É possível usar a função len() tanto em Strings quanto em listas
-# In Python, like in C, any non-zero integer value is true; zero is false. Anything with a non-zero length is true, empty sequences are false
-# Quando há uma declaração composta ou um bloco melhor dizendo, é necessário sempre pular uma linha deixando ela em branco para o compilador entender que aquela sequência terminou
-# Code that modifies a collection while iterating over that same collection can be tricky to get right. Instead, it is usually more straight-forward to loop over a copy of the collection or to create a new collection
-# Todo intervalo segue esse padrão (começo(incluso), fim(não-incluso), o quanto que vai pular)
-# The break statement breaks out of the innermost enclosing for or while loop. If a loop finishes without executing the break, the else clause executes
-# Quando usado dessa forma, o else faz mais papel de try exception do que um else por si só
-# You can use the class name followed by an argument list resembling a constructor, but with the ability to capture attributes into variables
-# Invés de importar a biblioteca dataclass, quando definindo funções pode apenas definir __match_args__ para dizer para o Python que a ordem a ser seguida é a mesma que definida ex. __match_args__ = ("x", "y")
-# Like unpacking assignments, tuple and list patterns have exactly the same meaning and actually match arbitrary sequences. An important exception is that they don’t match iterators or strings
-# The keyword def introduces a function definition. It must be followed by the function name and the parenthesized list of formal parameters
-# desempacotamento de tupla = Python primeiro calcula o lado direito inteiro
-# Todo tipo de função cria seu próprio espaço de variáveis
-# Python adiciona a um dicionário interno as funções criadas dessa maneira: nome da variável -> valor, essa tabela existe só durante a execução da função (temporariamente/local)
-# Mesmo que já exista uma variável "global", ou seja já foi definida no código, mesmo assim é possível criar uma variável com o mesmo nome que não será alterada
-# Primeiro, a função procura dentro dela mesma, depois ela procura no "Enclosing", ou seja dentro de uma nested function(Funções dentro de funções), depois global (definida ao longo do código) e por fim nas Built-ins, que são funções já pré-definidas pelo Python, como len() por exemplo
-
-# Uma função não consegue alterar o valor de uma variável global a não ser que use global váriavel
-# Para alterar no enclosing usa-se nonlocal
-# Essa diferença de variáveis ocorre, porquê quando refêrenciadas, as funções criam objetos diferentes, que se diferenciam das variáveis
-# Todo tipo de função sempre retorna algo, apesar de não ter um return, ela ainda retorna algo nulo. Esse é um padrão que todo tipo de dado mutável em python segue
-# A method is a function that ‘belongs’ to an object and is named obj.methodname, where obj is some object (this may be an expression), and methodname is the name of a method that is defined by the object’s type
-# Um exemplo de método é o append() de listas
-# The in keyword tests whether or not a sequence contains a certain value
-# Os valores padrão dos parâmetros são calculados no momento em que a função é criada, e não quando ela é chamada, dessa maneira a função reutiliza as variáveis
-# As default, functions share subsequent calls
-# In a function call, keyword arguments must follow positional arguments. All the keyword arguments passed must match one of the arguments accepted by the function
-# where / and * are optional. If used, these symbols indicate the kind of parameter by how the arguments may be passed to the function: positional-only, positional-or-keyword, and keyword-only (named parameters)
-# Por padrão se não existir / ou * a função aceita tanto por posição, quanto por variável
-# Use positional-only if you want the name of the parameters to not be available to the user
-# Use keyword-only when names have meaning and the function definition is more understandable by being explicit with names or you want to prevent users relying on the position of the argument being passed
-# For an API, use positional-only to prevent breaking API changes if the parameter’s name is modified in the future
-# O * “espalha” os elementos da lista como argumentos separados, desempacotando-os
-# Like nested function definitions, lambda functions can reference variables from the containing scope
-# Not all data can be sorted or compared. For instance, [None, 'hello', 10] doesn’t sort because integers can’t be compared to strings and None can’t be compared to other types.
-# Also, there are some types that don’t have a defined ordering relation. For example, 3+4j < 5+7j isn’t a valid comparison
-# Para adicionar algo em uma lista usa-se o método .append e para retirar usa-se .pop
-# Entretanto, para listas maiores que precisam de mais otimização é possível usar a coleção collections.deque para fazer a adição e a remoção de itens
-# a função del das listas também pode apagar o valor armazenado de uma variável para reutilizá-la novamente
-# Note that multiple assignment is really just a combination of tuple packing and sequence unpacking (tuplas diferentemente das listas, são imutáveis)
-# Dicionários podem ser criados de 3 formas: lista de chaves (métodos), compreensão e por meio de construção:
-# Use a comma-separated list of key: value pairs within braces: {'jack': 4098, 'sjoerd': 4127}, dict comprehension: {}, {x: x ** 2 for x in range(10)} & type constructor: dict(), dict([('foo', 100), ('bar', 200)])
-# To loop over two or more sequences at the same time, the entries can be paired with the zip() function
-# Para reverter um for, basta usar a função reversed antes do range
-# usar sorted(set()) em uma lista, é a maneira mais idiomática de organizar uma lista eliminando repetições e colocando em ordem alfabética
-# módulos são a base para escrever código limpo, reutilizável e escalável em Python, você cria arquivos e cita eles em outros
-# When you don’t need fancy output but just want a quick display of some variables for debugging purposes, you can convert any value to a string with the repr() or str() functions
-# For objects which don’t have a particular representation for human consumption, str() will return the same value as repr(). Many values, such as numbers or structures like lists and dictionaries, have the same representation using either function
-# The repr() of a string adds string quotes and backslashes (r string)
-# You can use placeholders like $x and replace them with values from a dictionary
-# Também pode usar números para substituir a ordem da posição referenciada na método str.format()
-# O método str.zfill() adiciona zeros a esquerda de uma string numérica
-# Strings são fáceis de salvar e ler em arquivos, mas números e estruturas complexas (listas aninhadas, dicionários) exigem mais trabalho. Por isso, o módulo json do Python simplifica isso
-# O módulo lida bem com listas e dicionários, mas serializar instâncias de classes exige esforço extra
-# In Python, there’s no direct "try-with-resources" keyword like in Java, but the with statement combined with context managers serves the same purpose
-# Existem 2 tipos de erros, sintaxe e exceções
-# o try e o except tem suas verificações executadas simultâneamente
-# A ordem dos excepts seguem a linha de código, então se um except for cumprido antes, outro não ocorre
-# Except também funciona em funções quando chamadas
-# a função raise serve para mostrar um erro
-# A função finally sempre acontece antes da verificação, por isso ocorre de qualquer maneira
-# A função with é um exemplo de clean-up actions
-# except* serve para aglomerar mais de uma exceção dentro de outra ao mesmo tempo
-# aliases behave like pointers in some respects
-# em python duas ou mais variáveis podem referenciar a um mesmo objeto da mesma classe
-# Em Python, não há ponteiros explícitos nem new/delete. O garbage collector cuida da liberação de memória automaticamente
-# copy.copy() → cópia rasa (shallow copy) e copy.deepcopy() → cópia profunda (deep copy)
-# Todo método em Python dentro de uma classe deve receber self como primeiro parâmetro, que representa a própria instância
-# In fact, local variables are already determined statically
-# Uma peculiaridade especial do Python é que – se nenhuma declaração global ou nonlocal estiver em vigor – atribuições a nomes sempre vão para o escopo mais interno
-# Parecido com as funções, as classes seguem também uma ordem de prioridade: escopo global -> atribuições global -> atribuições nonlocals -> atribuições locais
-
-# A instânciação usa a mesma notação das funções, só que no lugar de um parâmetro, ela retorna uma nova instância de uma classe
-# Todo tipo de classe precisa ter um método especial chamado __init__() por padrão
-# Data attributes correspond to “instance variables” in Smalltalk, and to “data members” in C++
-# Esses atributos recebem valor mesmo sem serem chamados
-# Quando você define uma função dentro de uma classe, ela se torna um método
-# Ao chamar esse método a partir de um objeto (instância da classe), o próprio objeto é automaticamente passado como o primeiro argumento da função (self)
-# Métodos de instância: Permite acessar e modificar atributos do objeto (self); Métodos de classe: construtores alternativos ou métodos que precisam manipular a classe em si (cls); Métodos estáticos: apenas por organização lógica
-# instance variables are for data unique to each instance and class variables are for attributes and methods shared by all instances of the class
-# Um exemplo prático é ao invés de criar uma lista que é compartilhada por todas as instâncias de uma classe, se esse não for o objetivo é claro, criar uma lista nova para cada nova instância, já que elas são mutáveis
-# A ordem de prioridade para atribuir valor a um atributo é primeiro a instância e depois a classe
-# self é uma convenção para padronizar e facilitar a localização das variáveis dentro de uma classe
-# inheritance serve para herdar/referenciar funções super ou sub umas as outras usando super()
-# Todos os métodos de python são do tipo virtual como o de C++
-# Python tem 2 funções de herança embutidas: isinstance() e issubclass()
-# Também é possível fazer heranças múltiplas, seguindo a ordem de referenciação da esquerda para a direita
-# Dataclasses are similar to the Pascal “record” or C “struct”
-# for statement calls iter() on the container object
-# When there are no more elements, __next__() raises a StopIteration exception which tells the for loop to terminate (next())
+# Comentários sobre o curso "Python Orientado a Objetos" do canal Curso em Vídeo
+# A "Crise de Software" surgiu em 1960
+# A característica que identifica as linguagens lineares são os desvios forçados com o "goto"
+# O Criador da linguagem "Smalltalk" foi Alan Kay
+# A linguagem simula foi um super conjunto da linguagem Algol60
+#  As duas últimas letras da sigla "OOAD" significam Analysis/Design
+# COMERN C-confiavel, O-oportuno, M-manutenivel, E-extensivo, R-reutilizavel e N-Natural
+# Alterações não muda outra área; Pode ser feito simultaneamente; Mais fácil de atualizar;
+# Sistema não estático (tudo pode crescer de forma útil); Utilizado em outros sistemas
+# Mais fácil de entender
+# (Classe = Molde) != Objeto
+# Nome, +-# atributos (coisas que a classe vai ter) e métodos +-# (coisas que eu posso fazer com a classe) ()
+# Instância = ação de instanciar/seguir o padrão da classe para criar o objeto
+# "Um objeto é uma instância de uma classe, definido por algo material/abstrato a partir de uma classe, descrito por seus atributos, métodos e estado"
+# Todo atributo tem valores de um estado
+# Ex. Objeto biscoito, instanciar a Classe molde de coração (colocar no molde), atributo inteiro, estado verdadeiro, método comer (muda o estado)
+# Atividade: Objeto1(concreto)-computador, classe marca hp, atributo ligado, estado verdadeiro / Objeto2(abstrato)-previsão do tempo, classe dia limpo, atributo chuva, estado falso
+# Aula 04
