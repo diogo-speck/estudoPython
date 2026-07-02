35deae8f606b8d8dfda19a989cacff3872b056a2
diogo-speck <diogospeck@gmail.com>
2026-07-02 12:42:44 -0300

    Termino da aula Curso Python POO: Aula 8 com a correção do exercício da aula 07 só que mudado para outra classe abstrata Academia

---
+++ cursoPOO/exercicios/ex005/academia.py
@@ -0,0 +1,33 @@
+from rich import print, inspect
+from classesEx005 import * # importando as classes do módulo classesEx004
+
+def main():
+    a1 = Aluno("José", 17, "Peito", "Segunda")
+    a1.estaNaAcademia()
+    a1.fazerAniversario()
+    print(a1.fazerMatricula())
+    print(a1.fazerMatricula())
+    print(a1.malhar("Costas", "Terça"))
+    # inspect(a1, methods=True) equivalente a (a1.__dict__)
+
+    p1 = Professor("Samuel", 37, "funcional", "mestre")
+    p1.estaNaAcademia()
+    p1.fazerAniversario()
+    print(p1.darAula())
+    # inspect(p1, methods=True)
+
+    f1 = Funcionario("Claúdia", 27, "auxiliar de limpeza", "serviços gerais")
+    f1.estaNaAcademia()
+    print(f1)
+    f1.fazerAniversario()
+    print(f1.baterPonto())
+    print(f1.baterPonto())
+    print(f1)
+    # inspect(f1.estaNaAcademia())
+
+    # x = Pessoa("Gustavo", 48) (não posso instanciar uma classe abstrata)
+
+
+
+if __name__ == "__main__": # boa prática para arquivos maiores e se quiser mudar o nome
+    main()
---
+++ cursoPOO/exercicios/ex005/classesEx005.py
@@ -0,0 +1,114 @@
+from abc import ABC, abstractmethod # Abstract Base Classes
+
+class Pessoa(ABC): # SuperClasse Abstrata
+    """
+        SuperClasse chamado Pessoa em uma academia abstrata que possui os parâmetros nome, idade e lista de alunos matriculados
+        ex. e1 = Pessoa()
+        Possui o método fazerAniversário(): 
+        ex. e1.fazerAniversário()
+    """
+    alunos = []
+    def __init__(self, nome="", idade=0):
+        self.nome = nome
+        self.idade = idade
+    
+    def fazerAniversario(self):
+        self.idade+=1
+
+    @abstractmethod # Método Obrigatório
+    def estaNaAcademia(self):
+        pass
+
+class Aluno(Pessoa): # SubClasse
+    """
+        SubClasse derivada da Class Pessoa, que instancia um objeto chamado Aluno e atribui a ele um nome e idade
+        (da super classe), além disso também dá a ele um treino, um dia da semana e inicializa-o como não-matriculado
+        ex. a1 = Aluno("nome", idade, "treino", "dia")
+        Possui 2 métodos, o método fazerAniversário() da classe Pessoa: 
+        ex. a1.fazerAniversário()
+        E o método fazerMatricula():
+        ex. print(a1.fazerMatricula())
+    """
+    def __init__(self, nome="", idade=0, treino="A", dia=""):
+        super().__init__(nome, idade)
+        self.treino = treino
+        self.dia = dia
+        self.matriculado = False
+
+    def fazerMatricula(self):
+        if self.matriculado:
+            return f"O(a) aluno(a) [green]{self.nome}[/] já está matriculado"
+        else:
+            self.matriculado = True
+            Pessoa.alunos.append(self.nome)
+            return f"O(a) aluno(a) [green]{self.nome}[/] foi matriculado"
+
+    def malhar(self, treinoatual="", diaatual=""):
+        if treinoatual == "":
+            treinoatual = self.treino
+        if diaatual == "":
+            diaatual = self.dia
+        print(f"O último treino do(a) {self.nome} foi {self.treino} na(o) {self.dia}")
+        self.treino, self.dia = treinoatual, diaatual
+        return f"Hoje é [blue]{diaatual}[/] e seu treino é [red]{treinoatual}"
+
+    def estaNaAcademia(self):
+        return f"O(a) aluno(a) {self.nome} está na academia treinando {self.treino} na {self.dia}"
+
+
+class Professor(Pessoa): # SubClasse
+    """
+        SubClasse derivada da Class Pessoa, que instancia um objeto chamado Professor/instrutor e atribui a ele um
+        nome e idade (da super classe), além disso também dá a ele um tipo de aula ex. funcional e um nível
+        (por padrão superior que é o mínimo para dar aula no Brasil)
+        ex. p1 = Professor("nome", idade, "tipo", "nivel")
+        Possui 2 métodos, o método fazerAniversário() da classe Pessoa: 
+        ex. p1.fazerAniversário()
+        E o método darAula():
+        ex. print(p1.darAula())
+    """
+    def __init__(self, nome="", idade=0, tipo="musculação", nivel="superior"):
+        super().__init__(nome, idade)
+        self.tipo = tipo
+        self.nivel = nivel
+
+    def darAula(self):
+        return f"O(a) professor(a) [black on white]{self.nome}[/] começou a dar aula de [orange]{self.tipo}[/] para o(s) aluno(s): {Aluno.alunos}"
+
+    def estaNaAcademia(self):
+        return f"O(a) instrutor(a) {self.nome} de nível {self.nivel} está na academia dando aula de {self.tipo}"
+
+
+class Funcionario(Pessoa): # SubClasse
+    """
+        SubClasse derivada da Class Pessoa, que instancia um objeto chamado Funcionário e atribui a ele um nome e idade
+        (da super classe), além disso também dá a ele um cargo, um setor e inicializa-o com o ponto não-batido
+        ex. f1 = Funcionario("nome", idade, "cargo", "setor")
+        Possui 3 métodos, o método fazerAniversário() da classe Pessoa: 
+        ex. f1.fazerAniversário()
+        O método baterPonto():
+        ex. print(f1.baterPonto())
+        E o dunder method __str__ para mostrar os atributos e o estado atual do funcionário:
+        ex. print(f1)
+    """
+    def __init__(self, nome="", idade=0, cargo="", setor=""):
+        super().__init__(nome, idade)
+        self.cargo = cargo
+        self.setor = setor
+        self.pontoBatido = False
+
+    def baterPonto(self):
+        if self.pontoBatido:
+            return f"O(a) funcionário(a) [purple]{self.nome}[/] já bateu o ponto"
+        else:
+            self.pontoBatido = True
+            return f"O(a) funcionário(a) [purple]{self.nome}[/] bateu o ponto"
+    
+    def __str__(self):
+        if self.pontoBatido:
+            return f"O(a) funcionário(a) {self.nome} está trabalhando no setor de {self.setor} como {self.cargo}"
+        else:
+            return f"O(a) funcionário(a) {self.nome} ainda não bateu o ponto"
+
+    def estaNaAcademia(self):
+        return f"O(a) funcionário(a) {self.nome} do setor de {self.setor} está na academia trabalhando como {self.cargo}"
