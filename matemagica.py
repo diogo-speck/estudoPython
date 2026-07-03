16258760c74d6bbae25db7e4e72cfb2984905750
diogo-speck <diogospeck@gmail.com>
2026-07-03 15:03:08 -0300

    Sequência de 3 cara/coroa com função criado

--- matemagica.py
+++ matemagica.py
@@ -1,3 +1,5 @@
+import os
+
 def confere_data(data):
     try:
         dia, mes, ano = data.split("/")
@@ -11,13 +13,45 @@ def confere_data(data):
     except:
         print("Data inválida")
 
+def moeda():
+    # Gera 0 (cara) ou 1 (coroa) sem usar random
+    return ord(os.urandom(1)) % 2
 
 
 
 data = input("Digite uma data para conferir se ela é válida ou não (dd/mm/yyyy): ")
 confere_data(data)
 
+#Coin flip sequence (HTT TTH)
+
+acertou = False
+gerados = ""
+contador = 0
+tentativas = 0
+todas = ""
+sequencia = input("Escolha uma sequência de 3 jogadas de cara/coroa (ex. HTT): ").upper()
+
+while not acertou:
+    while contador < 3:
+        if moeda() == 0:
+            gerados += "H"  # Cara
+        else:
+            gerados += "T"  # Coroa
+        contador += 1
+
+    print("Sequência gerada: ", gerados)
+    todas+=gerados
+
+    if gerados == sequencia:
+        print(f"🎉 Você acertou a sequência em {tentativas+1} tentativas!")
+        print(todas)
+        acertou = True
+    else:
+        gerados = ""
+        contador = 0
+        tentativas+=1
+
+
 # Todo Números narcisistas
 # Todo Chicken McNugget Theorem
-# Todo HTT TTH Coin flip sequence
 # Todo Algoritmo de Euclides
