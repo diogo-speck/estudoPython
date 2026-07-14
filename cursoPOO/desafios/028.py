8d418973ef2e6493df53c05aa1b9db087aa42f1b
diogo-speck <diogospeck@gmail.com>
2026-07-14 13:35:19 -0300

    Corrigindo o desafio 28 para ficar mais parecido com o exemplo

--- cursoPOO/desafios/028.py
+++ cursoPOO/desafios/028.py
@@ -7,12 +7,11 @@ class Termostato:
         Classe que instancia um objeto chamado Termostato que inicia em 24 °C e pode aumentar e diminuir a temperatura,
         além disso, tem um limite mínimo de temperatura (16 °C) e um máximo (30 °C)
         ex. t1 = Termostato()
-        Possui 2 métodos:
-        aumentar ou diminuir com + -
-        ex. t1.setTemp()
-        confere se o valor dito é múltiplo de 0,5 e está no intervalo de 16 e 30
-        ex. t1.getTemp()
-        vai aumentando ou diminuindo 0,5 até chegar no valor informado
+        Possui 1 method chamado ftemperatura para mostrar um painel com informações sobre a temperatura
+        ex. t1.ftemperatura()c
+        Além de um atributo validado @temperatura que confere se o valor dito é múltiplo de 0,5 e está no intervalo de
+        16 e 30, se não limita ao mínimo e ao máximo
+        ex. t1.temperatura = valor
     """
 
     temp_min: int = 16
@@ -21,21 +20,23 @@ class Termostato:
     def __init__(self):
         self.__temp = 24.0
 
-    def setTemp(self, temp=24.0):
-        try:
-            if round(temp * 2) % 1 == 0 and self.temp_min <= temp <= self.temp_max:
-                self.__temp = temp
-                return self.__temp
-            else:
-                print("Número fora do intervalo ou não múltiplo de 0,5")
-        except:
-            print("A temperatura informada não é um número")
-            return 0
-
-    def get_temp_value(self):
+    @property
+    def temperatura(self):
         return self.__temp
 
-    def getTemp(self):
+    @temperatura.setter
+    def temperatura(self, temp=24.0):
+        if (temp * 2) % 1 == 0 and self.temp_min <= temp <= self.temp_max:
+            self.__temp = temp
+        elif temp > self.temp_max:
+            self.__temp = self.temp_max
+        elif temp < self.temp_min:
+            self.__temp = self.temp_min
+        else:
+            print("Número não múltiplo de 0,5, temperatura mantida")
+
+
+    def ftemperatura(self):
         total_range = self.temp_max - self.temp_min
         progresso = int(((self.__temp - self.temp_min) / total_range) * 20)  # 20 blocos
 
@@ -47,29 +48,30 @@ class Termostato:
             cor = "red"
 
         barra = "█" * progresso + " " * (20 - progresso)
-        return Panel(
+        print(Panel(
             f"[blue]{self.temp_min}°C[/] [{cor}]{barra}[/] [red]{self.temp_max}°C[/]\nTemperatura atual: {self.__temp} °C",
-            title="Termostato", width=50)
+            title="Termostato", width=50))
 
 
 t1 = Termostato()
 
-t1.setTemp(25)
+t1.temperatura = 25.3 # Não muda porque não é múltiplo de 0,5
 inspect(t1, private=True, methods=True)
-print(f"A temperatura atual é {t1.get_temp_value()}")
+print(f"A temperatura atual é: ")
+t1.ftemperatura()
 
 while True:
     print(Panel("+ - Aumenta a Temperatura \n- - Diminui a Temperatura \n16-30 - Escolhe a Temperatura", title="Menu", width=40))
-    escolha = input("Escolha: ")
+    escolha = input("Escolha: ") # vai aumentando ou diminuindo 0,5 usando '+', ou '-'
     if escolha == "+":
-        t1.setTemp(t1.get_temp_value() + 0.5)
+        t1.temperatura += 0.5
     elif escolha == "-":
-        t1.setTemp(t1.get_temp_value() - 0.5)
+        t1.temperatura -= 0.5
     else:
         try:
             valor = float(escolha)
-            t1.setTemp(valor)
+            t1.temperatura = valor
         except ValueError:
             break
 
-    print(t1.getTemp())
+    t1.ftemperatura()
