c2df3300594d3006dc3c01c0e159a47ee7bba013
diogo-speck <diogospeck@gmail.com>
2026-08-18 16:18:04 -0300

    ex35 finalizado

--- cursoPOO/desafios/desafio035/035.py
+++ cursoPOO/desafios/desafio035/035.py
@@ -1,7 +1,17 @@
 from classes035 import *
 
 def __main__():
-    pass
+    a1 = DOCX("prova", 250_000)
+    a2 = PDF("contrato", 1_300_000)
+    a3 = PNG("logo", 1_200_000)
+    a4 = JPEG("foto", 300_000)
+    a5 = GIF("loaf_cat", 500_000)
+
+    a1.abrir()
+    a2.abrir()
+    a3.abrir()
+    a4.abrir()
+    a5.abrir()
 
 if __name__ == "__main__":
     __main__()
--- cursoPOO/desafios/desafio035/classes035.py
+++ cursoPOO/desafios/desafio035/classes035.py
@@ -1,16 +1,67 @@
-# Implemente uma Classe Funcionario, simulando um funcionário com seu salário e bônus salarial
+# Implemente uma Classe Arquivo, simulando a abertura de diferentes tipos de arquivos
 from rich import print, inspect
-from abc import ABC
+from abc import ABC, abstractmethod
+from random import random
 
-class Funcionario(ABC):
+class Arquivo(ABC):
     """
-        Classe abstrata que instancia um objeto chamado Funcionario onde recebe um nome e um salário
-        Possui os atributos @nome (público) e @salario (privado)
-        ex. f1 = Funcionario("nome", salario)
-        Possui 1 method:
-        calcular_bônus(valor)
-        ex. f1.calcular_bônus(valor)
+        Classe abstrata que instancia um objeto chamado Arquivo onde recebe um nome, a _extensao e o tamanho (em bytes)
+        Possui os atributos nome (público), #_extensao (protegido), tamanho (público) e @nome_completo (público)
+        ex. a1 = Arquivo("nome", "extensao", tamanho)
+        Possui 1 abstractmethod:
+        abrir()
+        ex. a1.abrir()
     """
 
-    def __init__(self):
+    def __init__(self, nome, extensao, tamanho):
+        self.nome = nome
+        self.__extensao = extensao
+        self.tamanho = tamanho
+    
+    @property
+    def nome_completo(self):
+        return f"'{self.nome}.{self.__extensao}'({self.tamanho/1000000}MB)"
+    
+    @abstractmethod
+    def abrir(self):
         pass
+
+
+class PDF(Arquivo):
+    def __init__(self, nome:str="novo_arquivo", tamanho:int=1000):
+        super().__init__(nome, "pdf", tamanho)
+
+    def abrir(self):
+        print(f"Abrindo o arquivo {self.nome_completo} no Adobe Reader")
+
+
+class DOCX(Arquivo):
+    def __init__(self, nome:str="novo_arquivo", tamanho:int=10000):
+        super().__init__(nome, "docx", tamanho)
+
+    def abrir(self):
+        print(f"Abrindo o arquivo {self.nome_completo} no Microsoft Word")
+
+
+class PNG(Arquivo):
+    def __init__(self, nome:str="novo_arquivo", tamanho:int=100):
+        super().__init__(nome, "png", tamanho)
+
+    def abrir(self):
+        print(f"Você abriu o arquivo {self.nome_completo} Selecione o programa de visualização de png (Adobe Photoshop, GIMP ou Paint)")
+
+
+class JPEG(Arquivo):
+    def __init__(self, nome:str="novo_arquivo", tamanho:int=1000):
+        super().__init__(nome, "jpeg", tamanho)
+
+    def abrir(self):
+        print(f"Você abriu o arquivo {self.nome_completo} Selecione o programa de visualização de imagens jpeg (Windows Fotos ou IrfanView)")
+
+
+class GIF(Arquivo):
+    def __init__(self, nome:str="novo_arquivo", tamanho:int=15000):
+        super().__init__(nome, "gif", tamanho)
+
+    def abrir(self):
+        print(f"Você abriu o arquivo {self.nome_completo} Selecione o programa de visualização de gifs (FastStone Image Viewer ou ScreenToGif)")
