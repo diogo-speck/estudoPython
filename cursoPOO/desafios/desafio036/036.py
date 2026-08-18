4614c0cf1005144459c88d02ea44a13f69f3b618
diogo-speck <diogospeck@gmail.com>
2026-08-18 17:37:40 -0300

    ex036 privado

--- cursoPOO/desafios/desafio036/036.py
+++ cursoPOO/desafios/desafio036/036.py
@@ -1,7 +1,8 @@
 from classes036 import *
 
 def __main__():
-    pass
+    finalizar_compra(Boleto(), 3_500)
+    #inspect(Pix(), all=True)
 
 if __name__ == "__main__":
     __main__()
--- cursoPOO/desafios/desafio036/classes036.py
+++ cursoPOO/desafios/desafio036/classes036.py
@@ -1,16 +1,69 @@
-# Implemente uma Classe Funcionario, simulando um funcionário com seu salário e bônus salarial
+# Implemente uma Classe Pagamento, simulando um pagamento de diferentes formas
 from rich import print, inspect
-from abc import ABC
+from abc import ABC, abstractmethod
 
-class Funcionario(ABC):
+class Pagamento(ABC):
     """
-        Classe abstrata que instancia um objeto chamado Funcionario onde recebe um nome e um salário
-        Possui os atributos @nome (público) e @salario (privado)
-        ex. f1 = Funcionario("nome", salario)
-        Possui 1 method:
-        calcular_bônus(valor)
-        ex. f1.calcular_bônus(valor)
+        Classe abstrata que instancia um objeto chamado Pagamento onde recebe um valor
+        Possui os atributos -_valor (privada) e +@fvalor (público)
+        ex. finalizar_compra(metodo, valor)
+        Possui 1 abstractmethod:
+        pagar(valor)
+        ex. p1.pagar()
     """
 
-    def __init__(self):
+    def __init__(self, valor:float=0.0):
+        self._valor = valor
+    
+    @abstractmethod
+    def pagar(self):
         pass
+
+    @property
+    def fvalor(self):
+        return f"R${self._valor:,.2f}"
+
+
+class Boleto(Pagamento):
+    def __init__(self, valor:float=0.0):
+        super().__init__(valor)
+        
+    def pagar(self):
+        print(f"Você pagou {self.fvalor} no boleto")
+
+
+class Pix(Pagamento):
+    def __init__(self, valor:float=0.0):
+        super().__init__(valor)
+        
+    def pagar(self):
+        print(f"Você pagou {self.fvalor} no pix")
+
+
+class Cartao(Pagamento):
+    def __init__(self, valor:float=0.0):
+        super().__init__(valor)
+        
+    def pagar(self):
+        tipo = input(f"Você escolheu pagar {self.fvalor} no cartão. Débito ou Crédito (d/c)? ")
+        if tipo == 'd':
+            print(f"{self.fvalor} debitado no seu cartão de débito")
+        else:
+            print(f"{self.fvalor} creditado no seu cartão de crédito")
+
+
+class Dinheiro(Pagamento):
+    def __init__(self, valor:float=0.0):
+        super().__init__(valor)
+        
+    def pagar(self):
+        troco = float(input(f"Você pagou {self.fvalor} no dinheiro. Troco para quanto? "))
+        if troco >= self._valor:
+            print(f"Seu troco será R${(troco-self._valor):,.2f} (Total: {self.fvalor})")
+        else:
+            print(f"Quantia inválida, não daremos troco!")
+
+    
+def finalizar_compra(metodo, valor):
+    metodo._valor = valor
+    metodo.pagar()
