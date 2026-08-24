e4b458a60ffb08b111659c518ccf88e123c77e84
diogo-speck <diogospeck@gmail.com>
2026-08-23 22:40:45 -0300

    finalizando o ex039

--- cursoPOO/desafios/desafio039/039.py
+++ cursoPOO/desafios/desafio039/039.py
@@ -1,7 +1,9 @@
 from classes039 import *
 
 def __main__():
-    pass
+    validar_dado(User(), "gus1234")
+    validar_dado(Email(), "adwfwfrg@gmail.com")
+    validar_dado(Senha(), "Testando123@")
 
 if __name__ == "__main__":
     __main__()
--- cursoPOO/desafios/desafio039/classes039.py
+++ cursoPOO/desafios/desafio039/classes039.py
@@ -1,16 +1,76 @@
-# Implemente uma Classe Funcionario, simulando um funcionário com seu salário e bônus salarial
+# Implemente uma Classe Validador, simulando a validação de dados
 from rich import print, inspect
-from abc import ABC
+from abc import ABC, abstractmethod
 
-class Funcionario(ABC):
+class Validador(ABC):
     """
-        Classe abstrata que instancia um objeto chamado Funcionario onde recebe um nome e um salário
-        Possui os atributos @nome (público) e @salario (privado)
-        ex. f1 = Funcionario("nome", salario)
-        Possui 1 method:
-        calcular_bônus(valor)
-        ex. f1.calcular_bônus(valor)
+        Classe abstrata que instancia um objeto chamado Validador
+        Não possui atributos mas possui o método validar()
     """
 
-    def __init__(self):
+    @abstractmethod
+    def validar(self):
         pass
+
+
+class User(Validador):
+    """
+    Restrições:
+    - Entre 5 e 20 caracteres
+    - Somente letras minúsculas
+    - Pode números, underline, hífen, mas não pode espaço
+    """
+    def validar(self, user:str):
+        if 5<=len(user)<=20 and (any(c.isupper() for c in user) == False) and (any(c==" " for c in user) == False):
+            print("[green]Usuário Válido")
+            return True
+        else:
+            print("[red]Usuário Inválido")
+            return False
+
+
+class Email(Validador):
+    """
+    Restrições:
+    - Deve conter somente 1 único @ e ter pelo menos 1 caracter antes
+    - Pode números, underline, hífen, mas não pode espaço
+    - O TLD (fim do domínio) precisa ter pelo menos 2 letras depois de um .
+    """
+    def validar(self, email:str):
+        email.lstrip("@")
+        try:
+            if (email.count("@") == 1) and (any(c==" " for c in email) == False) and len((email.split("@")[0]))>=1 and len((email.split("@")[1]).split(".")[1])>=2:
+                print("[green]E-mail Válido")
+                return True
+            else:
+                print("[red]E-mail Inválido")
+                return False
+        except:
+                print("[red]E-mail Inválido")
+                return False
+
+
+class Senha(Validador):
+    """
+    Restrições:
+    - Pelo menos 8 caracteres
+    - Pelo menos uma letra maiúsculas e um número
+    - Pelo menos um símbolo (exceto espaço)
+    """
+    def validar(self, senha:str):
+        sim = False
+        for i in range(len(senha)):
+            if senha[i] in '!#$%&*+-.=?@_':
+                sim = True
+                break
+        if 8<=len(senha) and (any(c.isupper() for c in senha) == True) and (any(c==" " for c in senha) == False) and (any(c.isdigit() for c in senha) == True) and sim:
+            print("[green]Senha Válida")
+            return True
+        else:
+            print("[red]Senha Inválida")
+            return False
+
+
+
+def validar_dado(tipo, valor):
+    tipo.validar(valor)
